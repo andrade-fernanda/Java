d8b7ecc3ebacf005b3dd8ea0af788eb203366b64
andrade-fernanda <fvw.andrade@gmail.com>
2022-07-20 14:28:48 -0300

    Projeto Final Bloco 1

---
+++ projetoFinalBloco1/UBS.java
@@ -0,0 +1,12 @@
+package projetoFinalBloco1;
+
+public interface UBS {
+	// MÉTODOS DA INTERFACE
+	
+			void temperatura(double testeTemperatura);
+			void avisoTemperatura();
+			void cartaoVac(boolean CartaoVac);
+				
+}
+
+
